1ecc2200c160456ea97cb157dcb5de8fb8b7b515
Ney Adrian <neyadrian2018@gmail.com>
2026-04-16 18:47:37 -0300

    update

--- src/br/edu/logistica/modelo/TipoCombustivel.java
+++ src/br/edu/logistica/modelo/TipoCombustivel.java
@@ -1,4 +1,9 @@
 package br.edu.logistica.modelo;
 
 public enum TipoCombustivel {
+    DIESEL(5.5),
+    GASOLINA(10.2),
+    ELETRICO(15.0)
+
+
 }
--- src/br/edu/logistica/modelo/Veiculo.java
+++ src/br/edu/logistica/modelo/Veiculo.java
@@ -1,4 +1,8 @@
 package br.edu.logistica.modelo;
 
 public abstract class Veiculo {
+    String placa;
+    double capacidadeCarga;
+
+    public enum
 }
