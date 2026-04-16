9d04a132f79b3c39d68ad3706134069591b80f7b
Ney Adrian <neyadrian2018@gmail.com>
2026-04-16 20:48:15 -0300

    update

--- src/br/edu/logistica/modelo/Caminhao.java
+++ src/br/edu/logistica/modelo/Caminhao.java
@@ -3,5 +3,10 @@ package br.edu.logistica.modelo;
 public class Caminhao extends Veiculo{
     private int eixos;
 
+    public Caminhao(String placa, double capacidadeCarga, int eixos) {
+        super(placa, capacidadeCarga);
+        this.eixos = eixos;
+    }
+
 
 }
--- src/br/edu/logistica/modelo/Veiculo.java
+++ src/br/edu/logistica/modelo/Veiculo.java
@@ -2,11 +2,17 @@ package br.edu.logistica.modelo;
 
 public abstract class Veiculo {
     private String placa;
-    private     double capacidadeCarga;
+    private double capacidadeCarga;
+
+    public Veiculo(String placa, double capacidadeCarga) {
+        this.placa = placa;
+        this.capacidadeCarga = capacidadeCarga;
+    }
 
     public enum StatusVeiculo {
         DISPONIVEL, EM_MANUTENCAO, EM_VIAGEM;
     }
+
 
     public abstract double calcularAutonimia();
 }
