5527d39d6543ea9e9f7b9d01b545c92395a251f3
neyadrian <neyadrian2018@gmail.com>
2026-04-17 15:55:54 -0300

    update

--- src/br/edu/logistica/modelo/Caminhao.java
+++ src/br/edu/logistica/modelo/Caminhao.java
@@ -1,10 +1,14 @@
 package br.edu.logistica.modelo;
 
+import br.edu.logistica.modelo.enuns.TipoCombustivel;
+
 public class Caminhao extends Veiculo{
     private int eixos;
 
-    public Caminhao(String placa, double capacidadeCarga, int eixos) {
-        super(placa, capacidadeCarga);
+    private static final int CAPACIDADE_TANQUE = 400;
+
+    public Caminhao(String placa, double capacidadeCarga, TipoCombustivel combustivel, int eixos) {
+        super(placa, capacidadeCarga, combustivel);
         this.eixos = eixos;
     }
 
---
+++ src/br/edu/logistica/modelo/Monitoriavel.java
@@ -0,0 +1,8 @@
+package br.edu.logistica.modelo;
+
+public interface Monitoriavel {
+
+    void enviarCoordenadas();
+    String obterLocalizacaoAtual();
+
+}
