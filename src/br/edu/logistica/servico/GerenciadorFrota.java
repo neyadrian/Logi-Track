525f84b0739dd8f2ed19662fe4d63039e1904cb2
neyadrian <neyadrian2018@gmail.com>
2026-04-17 16:08:10 -0300

    update

--- src/br/edu/logistica/servico/GerenciadorFrota.java
+++ src/br/edu/logistica/servico/GerenciadorFrota.java
@@ -1,4 +1,21 @@
 package br.edu.logistica.servico;
 
+import br.edu.logistica.modelo.Veiculo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GerenciadorFrota {
+
+    private final List<Veiculo> frota;
+    private final Map<String, Veiculo> indicePorPlaca;
+
+    public GerenciadorFrota() {
+        this.frota = new ArrayList<>();
+        this.indicePorPlaca = new HashMap<>();
+    }
+
+
 }
