6f61e0feab7f698dc8f5080c1707a4af7a7d094e
neyadrian <neyadrian2018@gmail.com>
2026-04-17 16:03:05 -0300

    update

--- src/br/edu/logistica/modelo/Van.java
+++ src/br/edu/logistica/modelo/Van.java
@@ -1,6 +1,17 @@
 package br.edu.logistica.modelo;
 
-public class Van extends Veiculo{
+import br.edu.logistica.modelo.enuns.TipoCombustivel;
+
+public class Van extends Veiculo {
     private boolean possuiRefrigeracao;
+
+    private static final int CAPACIDADE_TANQUE = 60;
+    private static final double PENALIDADE_REFRIGERACAO = 0.15;
+
+    public Van(String placa, double capacidadeCarga, TipoCombustivel tipoCombustivel, boolean possuiRefrigeracao) {
+        super(placa, capacidadeCarga, tipoCombustivel);
+        this.possuiRefrigeracao = possuiRefrigeracao;
+    }
+
 
 }
