a88241e0b823d3f3424d33b32b95621630f40753
neyadrian <neyadrian2018@gmail.com>
2026-04-17 15:42:16 -0300

    update

--- src/br/edu/logistica/modelo/Veiculo.java
+++ src/br/edu/logistica/modelo/Veiculo.java
@@ -15,5 +15,37 @@ public abstract class Veiculo {
         DISPONIVEL, EM_MANUTENCAO, EM_VIAGEM;
     }
 
-    public abstract double calcularAutonimia();
+    public abstract double calcularAutonomia();
+
+    public void exibirAutonomia() {
+        System.out.println("=== Informações do Veículo ===");
+        System.out.println("Placa: " + placa);
+        System.out.println("Capacidade de Carga: " + capacidadeCarga + " ton");
+        System.out.println("Status: " + statusVeiculo);
+        System.out.println("Autonomia estimada: " + calcularAutonomia() + " km");
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public double getCapacidadeCarga() {
+        return capacidadeCarga;
+    }
+
+    public void setCapacidadeCarga(double capacidadeCarga) {
+        this.capacidadeCarga = capacidadeCarga;
+    }
+
+    public StatusVeiculo getStatusVeiculo() {
+        return statusVeiculo;
+    }
+
+    public void setStatusVeiculo(StatusVeiculo statusVeiculo) {
+        this.statusVeiculo = statusVeiculo;
+    }
 }
