cfdd680d2f892d38518be3a9d4bddf9b7a69c9a2
neyadrian <neyadrian2018@gmail.com>
2026-04-17 15:40:14 -0300

    update

--- src/br/edu/logistica/modelo/Van.java
+++ src/br/edu/logistica/modelo/Van.java
@@ -1,4 +1,6 @@
 package br.edu.logistica.modelo;
 
 public class Van extends Veiculo{
+    private boolean possuiRefrigeracao;
+
 }
--- src/br/edu/logistica/modelo/Veiculo.java
+++ src/br/edu/logistica/modelo/Veiculo.java
@@ -3,10 +3,12 @@ package br.edu.logistica.modelo;
 public abstract class Veiculo {
     private String placa;
     private double capacidadeCarga;
+    private StatusVeiculo statusVeiculo;
 
     public Veiculo(String placa, double capacidadeCarga) {
         this.placa = placa;
         this.capacidadeCarga = capacidadeCarga;
+        this.statusVeiculo = StatusVeiculo.DISPONIVEL;
     }
 
     public enum StatusVeiculo {
