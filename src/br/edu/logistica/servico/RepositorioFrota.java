05e3c80d114032c1c1b99817b67978c94d072f55
Ney Adrian <neyadrian2018@gmail.com>
2026-05-28 16:05:23 -0300

    Aplicando o principio Single Responsibility Principle do SOLID, para dividir as responsabilidades do gerenciador de frota - RepositorioFrota Update

--- src/br/edu/logistica/servico/RepositorioFrota.java
+++ src/br/edu/logistica/servico/RepositorioFrota.java
@@ -16,4 +16,25 @@ public class RepositorioFrota {
         this.frota = new ArrayList<>();
         this.indicePorPlaca = new HashMap<>();
     }
+
+    public void adicionar(Veiculo v) {
+        if (indicePorPlaca.containsKey(v.getPlaca())) {
+            System.out.println("AVISO - Veículo com placa " + v.getPlaca() + " já cadastrado.");
+            return;
+        }
+        frota.add(v);
+        indicePorPlaca.put(v.getPlaca(), v);
+    }
+
+    public Veiculo buscarPorPlaca(String placa) {
+        return indicePorPlaca.get(placa);
+    }
+
+    public List<Veiculo> obterTodos() {
+        return new ArrayList<>(frota);
+    }
+
+    public int getTotalVeiculos() {
+        return frota.size();
+    }
 }
