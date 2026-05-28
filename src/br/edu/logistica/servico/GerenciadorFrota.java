1a40196b6211dbdbb1cfc7a595084a6461e31140
Ney Adrian <neyadrian2018@gmail.com>
2026-05-28 18:13:47 -0300

    OBS: a interface é o principio Dependecy Inversio Priciple do SOLID

--- src/br/edu/logistica/servico/GerenciadorFrota.java
+++ src/br/edu/logistica/servico/GerenciadorFrota.java
@@ -1,5 +1,7 @@
 package br.edu.logistica.servico;
 
+import br.edu.logistica.modelo.Veiculo;
+
 public class GerenciadorFrota {
     private final RepositorioFrota repositorio;
     private final RelatorioServico relatorios;
@@ -13,4 +15,19 @@ public class GerenciadorFrota {
         this.calculadora = calculadora;
     }
 
+    public void adicionarVeiculo(Veiculo v) {
+        repositorio.adicionar(v);
+        System.out.println("Veículo " + v.getPlaca() + " adicionado à frota.");
+    }
+
+    public void exibirFrotaCompleta() {
+        System.out.println("\n========= FROTA COMPLETA=========");
+        var veiculos = repositorio.obterTodos();
+        System.out.println("Total de veículos: " + veiculos.size());
+        System.out.println();
+        veiculos.forEach(Veiculo::exibirInformacoes);
+    }
+
+    public RelatorioServico getRelatorios() { return relatorios; }
+    public RastreamentoServico getRastreamento() { return rastreamento; }
 }
