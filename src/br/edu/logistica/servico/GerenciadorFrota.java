2c6794389d943e80faab1f548b6490b24483b56c
Ney Adrian <neyadrian2018@gmail.com>
2026-05-28 18:09:58 -0300

    Aplicando o principio Single Responsibility Principle do SOLID, para dividir as responsabilidades do gerenciador de frota - Interface CalcularAutonomia e Refatoração do Gerenciador Frota

--- src/br/edu/logistica/servico/GerenciadorFrota.java
+++ src/br/edu/logistica/servico/GerenciadorFrota.java
@@ -6,7 +6,7 @@ public class GerenciadorFrota {
     private final RastreamentoServico rastreamento;
     private final CalculadoraAutonomia calculadora;
 
-    public GerenciadorFrotaRefatorado( RepositorioFrota repositorio, RelatorioServico relatorios, RastreamentoServico rastreamento, CalculadoraAutonomia calculadora) {
+    public GerenciadorFrota( RepositorioFrota repositorio, RelatorioServico relatorios, RastreamentoServico rastreamento, CalculadoraAutonomia calculadora) {
         this.repositorio = repositorio;
         this.relatorios = relatorios;
         this.rastreamento = rastreamento;
